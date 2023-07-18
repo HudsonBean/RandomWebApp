3acb7ee693bb71b661f3eac62931791ded8abe7a
Hudson <hudsonbeandev@gmail.com>
2023-07-18 11:35:09 -0500

    Created client

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
+const fs = require("fs");
 
 //Variables
 const app = express();
@@ -10,6 +11,12 @@ const PORT = process.env.PORT || 8080;
 //Functions
 
 //Main
+
+//Set client proxy
+var file = "../client/package.json";
+var package = JSON.parse(fs.readFileSync(file));
+package.proxy = `http://localhost:${PORT}`;
+fs.writeFileSync(file, JSON.stringify(package));
 
 // Middleware
 app.use(express.json());
@@ -22,7 +29,7 @@ app.listen(PORT, ()=>{console.log(`Server running at \x1b[34;1;4mhttp://localhos
 // Routing
 // GETs
 app.get("/api/dev", (req, res) => {
-  res.status(200).json("Hello From Server!");
+  res.status(200).json({ body: "Hello From Server!" });
 });
 
 // POSTs
